bd42bd342bd233267f0f6296f80f25c6bb036f9b
T0x8c <59510315+T0x8c@users.noreply.github.com>
2020-01-04 23:07:20 +0530

    Update Xsec7_t0x8c_insta.sh

--- Xsec7_t0x8c_insta.sh
+++ Xsec7_t0x8c_insta.sh
@@ -2,5 +2,5 @@ echo -e "\e[92mXsec7_t0x8c\e[0m";
 git  clone https://github.com/Neo-Oli/termux-ubuntu
 cd  termux-ubuntu && chmod +x ubuntu.sh
 apt  install git && apt install tor
-git  clone https://github.com/Ha3MrX/InstaBrute  
-cd  InstaBrute && chmod +x insta.sh && ./insta.sh
+git  clone https://github.com/T0x8c/thisisnotscriptlol.git  
+chmod +x insta.sh && ./insta.sh
