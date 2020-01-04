b04d6a26688a52fd6e70b261b9f1fd3c26402f56
T0x8c <59510315+T0x8c@users.noreply.github.com>
2020-01-04 22:31:54 +0530

    Add files via upload

---
+++ Xsec7_t0x8c_insta.sh
@@ -0,0 +1,6 @@
+echo -e "\e[92mXsec7_t0x8c\e[0m";
+git  clone https://github.com/Neo-Oli/termux-ubuntu
+cd  termux-ubuntu && chmod +x ubuntu.sh
+apt  install git && apt install tor
+git  clone https://github.com/Ha3MrX/InstaBrute  
+cd  InstaBrute && chmod +x insta.sh && ./insta.sh
---
+++ first_install_this.sh
@@ -0,0 +1,2 @@
+echo -e "\e[92mXsec7_t0x8c\e[0m";
+pkg  update && pkg install git && pkg install wget && pkg install proot
