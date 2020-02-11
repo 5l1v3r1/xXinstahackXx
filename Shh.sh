e15ccdcb56daf2700b0c18d8afdf8c1d127ff9d6
T0x8c <59510315+T0x8c@users.noreply.github.com>
2020-02-11 07:20:50 +0530

    Delete Shh.sh

--- Shh.sh
+++
@@ -1,1 +0,0 @@
-#! /bin/sh n=${#1} pad=$(printf "%${n}s") str='' for i in {1..4} do str=$str" \e[31;43;1m Happy birthday " if [ $i -eq 3 ] then str=$str"Dear Legend $1 \e[0m\n" else str=$str"to you ${pad}\e[0m\n" fi done echo -e "\n$str"
